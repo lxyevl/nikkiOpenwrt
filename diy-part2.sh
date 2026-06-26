f3e464c013ba7e119c17086397f1e3ebd82a2b7f
lxyevl <lxyevl@gmail.com>
2026-06-26 18:15:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,23 +10,24 @@
 # See /LICENSE for more information.
 #
 
-#!/bin/bash
+# 纠正为 padavanonly 源码的标准设备代号
+echo "CONFIG_TARGET_mediatek=y" >> .config
+echo "CONFIG_TARGET_mediatek_filogic=y" >> .config
+echo "CONFIG_TARGET_mediatek_filogic_DEVICE_jcg_q30-pro=y" >> .config
 
-# 删除HomeProxy
+# 切除homeproxy和upnp
 echo "CONFIG_PACKAGE_luci-app-homeproxy-not=y" >> .config
 echo "CONFIG_PACKAGE_sing-box-not=y" >> .config
-
-# 剔除 UPnP
 echo "CONFIG_PACKAGE_luci-app-upnp-not=y" >> .config
 echo "CONFIG_PACKAGE_miniupnpd-not=y" >> .config
 
-# 添加 Nikki
-#echo "CONFIG_PACKAGE_luci-app-nikki=y" >> .config
-#echo "CONFIG_PACKAGE_luci-i18n-nikki-zh-cn=y" >> .config
-#echo "CONFIG_PACKAGE_nikki=y" >> .config
-#echo "CONFIG_PACKAGE_mihomo=y" >> .config
+#  Nikki 
+echo "CONFIG_PACKAGE_luci-app-nikki=y" >> .config
+echo "CONFIG_PACKAGE_luci-i18n-nikki-zh-cn=y" >> .config
+echo "CONFIG_PACKAGE_nikki=y" >> .config
+echo "CONFIG_PACKAGE_mihomo=y" >> .config
 
-# 透明代理和 nftables 转发必不可少的内核网络驱动
+# 补全透明代理网络驱动
 echo "CONFIG_PACKAGE_kmod-nft-tproxy=y" >> .config
 echo "CONFIG_PACKAGE_kmod-nft-socket=y" >> .config
 echo "CONFIG_PACKAGE_kmod-tun=y" >> .config
